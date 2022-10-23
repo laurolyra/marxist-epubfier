7181e2723a989be83037a0d3ea72ef41965a6a8c
laurolyra <laurolyra@gmail.com>
2022-10-23 10:40:09 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+app.listen(port, function () {
+    return console.log("Expressss is listening at http://localhost:".concat(port));
+});
