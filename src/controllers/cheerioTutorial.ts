266da5d021032eed4aa7df1c01967e3af3e14539
Lauro Lyra <laurolyra@gmail.com>
2023-03-05 23:22:48 -0300

    F - sending res. new variables

--- src/controllers/cheerioTutorial.ts
+++ src/controllers/cheerioTutorial.ts
@@ -1,23 +1,25 @@
 import { Request, Response } from "express";
-import { TARGET_URL } from "../variables";
+import {  TARGET_URL_CHAPTERS } from "../variables";
 import * as cheerio from "cheerio";
 import axios from "axios";
 import * as fs from 'fs';
 
 export const getChapters = ($) => { 
-	const chapterLinks = $('p[class*=toc]');
+	const tableOfContents = $('p[class*=toc]');
 	const chapterList = [];
-	chapterLinks.each((i, el) =>
-    $(el).find('a').each(function(i, link){chapterList.push($(link).attr('href'))})
+	tableOfContents.each((i: number, el: object) =>
+    $(el).find('a').each(function(i: number, link: object){chapterList.push($(link).attr('href'))})
   )
   const chapterURLSet = new Set(chapterList);
   // console.log('after push:', new Set(chapterList))
   chapterURLSet.forEach(item => console.log('item:', item));
 }
 
-export const chapterScrapper = (req, res) => 
-  axios.get(TARGET_URL).then((res: any) => {
+export const chapterScrapper = (req, res) => {
+  axios.get(TARGET_URL_CHAPTERS).then((res: any) => {
     const body = res.data;
     const $ = cheerio.load(body);
     getChapters($);
   });
+  res.status(200).send('Scrape ok!');
+}
--- src/variables.ts
+++ src/variables.ts
@@ -1,3 +1,3 @@
-// export const TARGET_URL = 'https://www.marxists.org/portugues/marx/1848/ManifestoDoPartidoComunista/index.htm';
-export const TARGET_URL = 'https://www.marxists.org/portugues/marx/1867/capital/';
-// export const TARGET_URL = 'https://www.marxists.org/portugues/marx/1866/08/instrucoes.htm';
+export const TARGET_URL_CHAPTERS = 'https://www.marxists.org/portugues/marx/1848/ManifestoDoPartidoComunista/index.htm';
+export const TARGET_URL_TOMES_CHAPTERS = 'https://www.marxists.org/portugues/marx/1867/capital/index.htm';
+export const TARGET_URL_NO_CHAPTERS = 'https://www.marxists.org/portugues/marx/1866/08/instrucoes.htm';
